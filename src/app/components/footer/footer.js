f8012c29c6eee175da537b67a2245faaafb585bc
Hayden Tofts <haydenized@gmail.com>
2023-06-30 10:41:44 -0400

    Add social links to footer

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -1,7 +1,25 @@
+import styles from "@/app/components/footer/footer.module.scss";
+
 function Footer() {
 	return (
 		<footer>
 			<div className="container">
+				<div className={styles.icons_container}>
+					<a
+						className={styles.icons_github}
+						href="https://github.com/CaymamCruncher/threads"
+						target="_blank"
+					>
+						Github
+					</a>
+					<a
+						className={styles.icons_linkedin}
+						href="https://www.linkedin.com/in/hayden-tofts-97b82517b/"
+						target="_blank"
+					>
+						LinkedIn
+					</a>
+				</div>
 				<small>&copy;2023 Threads All Rights Reserved</small>
 			</div>
 		</footer>
